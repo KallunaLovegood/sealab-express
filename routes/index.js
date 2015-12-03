5d1e91a0d1ace25d700fa7974faf178583219e04
Travis Brim <me@travisbrim.com>
2015-12-03 14:30:11 -0600

    blog_target_resolved
    resolved target="_blank" issue, added correct links to logo (prevented
    from functioning due to smoothscroll.js)

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/homepage', { title: 'SeaLab' });
+  res.render('pages/homepage', { title: 'SeaLab', logoLink: '/#topofpage' });
 });
 
 /* GET projects. */
 router.get('/projects', function(req, res, next) {
-  res.render('pages/projects', { title: 'SeaLab | Projects' });
+  res.render('pages/projects', { title: 'SeaLab | Projects', logoLink: './'});
 });
 
 /* GET Loom & Leaf. */
 router.get('/loomandleaf', function(req, res, next) {
-  res.render('pages/loomandleaf', { title: 'SeaLab | Loom & Leaf', previous: './sailsjs', next: './snapkitchen' });
+  res.render('pages/loomandleaf', { title: 'SeaLab | Loom & Leaf', logoLink: './', previous: './sailsjs', next: './snapkitchen' });
 });
 
 /* GET Snap Kitchen. */
 router.get('/snapkitchen', function(req, res, next) {
-  res.render('pages/snapkitchen', { title: 'SeaLab | Snap Kitchen', previous: './loomandleaf', next: './sailsjs' });
+  res.render('pages/snapkitchen', { title: 'SeaLab | Snap Kitchen', logoLink: './', previous: './loomandleaf', next: './sailsjs' });
 });
 
 /* GET Sails. */
 router.get('/sailsjs', function(req, res, next) {
-  res.render('pages/sailsjs', { title: 'SeaLab | Sails.js', previous: './snapkitchen', next: './loomandleaf' });
+  res.render('pages/sailsjs', { title: 'SeaLab | Sails.js', logoLink: './', previous: './snapkitchen', next: './loomandleaf' });
 });
 
 module.exports = router;
